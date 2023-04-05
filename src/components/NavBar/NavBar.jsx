98b72d7a5bc722de59e61ca340f22818822ab999
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-04-05 22:34:02 +0200

    navbar link added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,6 +8,20 @@ function NavBar() {
       <a href="/">
         <img className="logo" src={logo} alt="Astronomy logo" />
       </a>
+      <div className="navigation">
+        <a
+          className="navigation-link"
+          href="https://static.nationalgeographic.fr/files/styles/image_3200/public/02-trappist-planets.adapt_.1900.1.jpg?w=1600&h=900"
+        >
+          CELESTIAL WONDERS
+        </a>
+        <a className="navigation-link" href="/">
+          ABOUT US
+        </a>
+        <a className="navigation-link" href="/">
+          CONTACT
+        </a>
+      </div>
     </div>
   );
 }
