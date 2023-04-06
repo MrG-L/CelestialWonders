80ad129f75e02adb4a3c4afc4648813cd390aff3
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-04-06 14:26:00 +0200

    footer construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
-import NavBar from './components/NavBar/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar';
 import CelestialList from './components/celestial-cards-home/CelestialList';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <CelestialList />
+      <Footer />
     </div>
   );
 }
